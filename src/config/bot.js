e481c32530cf2c0d8fb2b28229affbd974158696
rbgaming90-png <rbgaming90@gmail.com>
2026-05-12 20:59:50 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "DARK SIDE ☠️",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#6b6a6a", 
+      secondary: "#0a0314", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
