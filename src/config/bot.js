c1dfb7580a4792f8c006616da492add85fb538da
rbgaming90-png <rbgaming90@gmail.com>
2026-05-12 22:10:00 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "DARK SIDE ☠️ / LOGS 🛡️ / VERIFICATION ✅",
+        name: "DARK SIDE ☠️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
